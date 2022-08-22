f5cb71f85c99628a3dbd2e4c222fb6b1cd524ad7
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-22 19:46:00 -0300

    Working with button, bannerand install CLSX

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Banner from '../../components/Banner';
 
 const Home = () => {
-  return <div className="text-white">Home</div>;
+  return (
+    <div className="text-white">
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;

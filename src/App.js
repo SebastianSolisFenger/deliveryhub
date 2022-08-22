102cf07b1dd8afee3e3086b1b4f47f24b7d8b8db
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-22 17:29:56 -0300

    Add Routing and ASSETS FOLDER

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
+import Navigation from './navigation';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navigation />
     </div>
   );
 }

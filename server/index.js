91da80d27fa1e1b56bb8c89f651ab5a96cf5f6ac
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-24 18:21:38 -0300

    configure main INDEX.JS with the database at least with ProductRouter so far

--- server/index.js
+++ server/index.js
@@ -8,3 +8,25 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
+const ProductRouter = require('./routes/productRouter.jsx');
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+db.on('error', console.error.bind(console, 'Mongodb connection error:'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Food Order!' });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use('/api/', ProductRouter);

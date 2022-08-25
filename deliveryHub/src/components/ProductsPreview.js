e114a30d74ac1580bc98553e891799b7e34a51c9
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-25 16:49:58 -0300

    map through the products data.data

--- deliveryHub/src/components/ProductsPreview.js
+++ deliveryHub/src/components/ProductsPreview.js
@@ -8,13 +8,17 @@ const ProductsPreview = () => {
   useEffect(() => {
     fetch("http://localhost:8080/api/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data))
+      .then((data) => setProducts(data?.data))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="container mx-auto pb-4 w-2/3 text-white">
       <h2>Products</h2>
+      {products.length > 0 &&
+        products.map((product, index) => {
+          return <div key={index}>{product.name}</div>;
+        })}
     </div>
   );
 };

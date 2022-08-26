4b5a928be2f3c0d4b79f7d2dbddda3957c3a9f12
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-26 14:24:38 -0300

    Add =>  <Provider store={store}>

--- deliveryHub/src/stores/rootStore.js
+++ deliveryHub/src/stores/rootStore.js
@@ -3,18 +3,13 @@ import rootReducer from "../stores/rootReducer";
 //import redux thunk as we need our actions to be async
 import thunk from "redux-thunk";
 
-
- /* eslint-disable no-underscore-dangle */
+/* eslint-disable no-underscore-dangle */
 const store = configureStore(
   {
     reducer: rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   },
   applyMiddleware(thunk)
 );
 /* eslint-enable */
-
-
-
 
 export default store;

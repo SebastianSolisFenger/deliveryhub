d15e685dc3958b1bea38e3e690b6e33d6fe7041c
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-29 17:37:22 -0300

    Done some cleaning

--- deliveryHub/src/pages/Menu/index.jsx
+++ deliveryHub/src/pages/Menu/index.jsx
@@ -5,7 +5,7 @@ import {
   selectAllProducts,
 } from "../../stores/menu/productSlice";
 import { useEffect } from "react";
-import ProductDetailCard from '../../components/ProductDetailCard'
+import ProductDetailCard from "../../components/ProductDetailCard";
 
 const Menu = () => {
   const dispatch = useDispatch();
@@ -22,17 +22,8 @@ const Menu = () => {
     <div className="bg-white">
       <h1 className="text-white">whatt</h1>
       <div className="menu-wrapper">
-        {/* {products.products.map((product, index) => {
-          console.log(product);
-          return (
-            <>
-              <div className="text-white">{product.name.name}</div>
-              <div className="text-white">{product.products[0].name}</div>
-            </>
-          );
-        })} */}
         {products.products[0].products.map((product, index) => {
-          return  <ProductDetailCard key={index}  product={product}/>
+          return <ProductDetailCard key={index} product={product} />;
         })}
       </div>
     </div>

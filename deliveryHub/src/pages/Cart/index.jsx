4eaa38ac51d35cc95b2a4672d87bf1074c821817
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-31 13:22:06 -0300

    setting up cart page

--- deliveryHub/src/pages/Cart/index.jsx
+++ deliveryHub/src/pages/Cart/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { Tabs } from '../../components/Tabs';
+import Button from '../../components/elements/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAllProducts } from '../../stores/menu/productSlice';
+import { ReactComponent as ArrowRightSvg } from '../../assets/icons/arrow-right-long-svgrepo-com.svg';
 
 const Cart = () => {
+  const cart = useSelector(selectAllProducts);
+  const tabs = ['Summary', 'Delivery', 'Payment'];
+
+  if (!cart || cart.products.length === 0) {
+    return (
+      <div className="bg-white text-black h-full flex justify-center p-4">
+        <h1> Your cart is empty!</h1>
+      </div>
+    );
+  }
   return <div>Cart</div>;
 };
 

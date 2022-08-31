95a3e47c016db5edb8bcd1cae02f9068d3a95254
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-31 17:23:03 -0300

    Work on => AddressForm

---
+++ deliveryHub/src/components/AddressForm.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Button from './elements/Button';
+import { ReactComponent as ArrowRightSvg } from '../assets/icons/arrow-right-long-svgrepo-com.svg';
+
+const AddressForm = () => {
+  // register would save the values that the user inputs in the form
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      className="md:w-2/3 md:mx-auto px-3 pt-1"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h3 className="pt-4 text-2xl md:text-center">Addre for the delivery</h3>
+      <div className="mb-4">
+        <label
+          className="block mb-2 text-sm font-bold text-gray-700"
+          for="streetAddress"
+        >
+          streetAddress
+        </label>
+        <input
+          {...register('address')}
+          className="w-full px-3 py-2 mb-3  text-sm loading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="street address"
+          type="text"
+          placeholder="Street Address"
+        />
+      </div>
+      <div className="mb-4 md:flex md:justify-between">
+        <div className="mb-4 md:mr-2 md:mb-0 flex-1">
+          <label
+            className="block mb-2 text-sm font-bold text-gray-700"
+            for="city"
+          >
+            city
+          </label>
+          <input
+            {...register('city')}
+            className="w-full px-3 py-2 mb-3  text-sm loading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="city"
+            type="text"
+            placeholder="City"
+          />
+        </div>
+
+        <div className="mb-4 md:mr-2 md:mb-0 flex-1">
+          <label
+            className="block mb-2 text-sm font-bold text-gray-700"
+            for="state"
+          >
+            State
+          </label>
+          <input
+            {...register('state')}
+            className="w-full px-3 py-2 mb-3  text-sm loading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="state"
+            type="text"
+            placeholder="State"
+          />
+        </div>
+      </div>
+
+      {/* COUNTRY AND POSTALCODE */}
+      <div className="mb-4 md:flex md:justify-between">
+        <div className="mb-4 md:mr-2 md:mb-0 flex-1">
+          <label
+            className="block mb-2 text-sm font-bold text-gray-700"
+            for="country"
+          >
+            Country
+          </label>
+          <input
+            {...register('country')}
+            className="w-full px-3 py-2 mb-3  text-sm loading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="country"
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+
+        <div className="mb-4 md:mr-2 md:mb-0 flex-1">
+          <label
+            className="block mb-2 text-sm font-bold text-gray-700"
+            for="postalCode"
+          >
+            Postal Code
+          </label>
+          <input
+            {...register('postalCode')}
+            className="w-full px-3 py-2 mb-3  text-sm loading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            id="postal code"
+            type="text"
+            placeholder="Postal Code"
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-end p-2">
+        <Button variant="dark" type="submit" className="flex items-center">
+          <span className="mr-1">Next</span>
+          <ArrowRightSvg />
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddressForm;
--- deliveryHub/src/pages/Cart/index.jsx
+++ deliveryHub/src/pages/Cart/index.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { cartProducts } from '../../stores/cart/cartSlice';
 import useTabSwitch from '../../hooks/useTabSwitch';
 import { ReactComponent as ArrowRightSvg } from '../../assets/icons/arrow-right-long-svgrepo-com.svg';
+import AddressForm from '../../components/AddressForm';
 
 const Cart = () => {
   const cart = useSelector(cartProducts);
@@ -21,6 +22,15 @@ const Cart = () => {
   return (
     <div className="bg-white h-screen text-black mx-auto mt-2 border  border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
       <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab} />
+      <div className={`tabs ${currentTab !== 'Summary' ? 'hidded' : ''}`}>
+        Summary
+      </div>
+      <div className={`tabs ${currentTab !== 'Delivery' ? 'hidded' : ''}`}>
+        <AddressForm />
+      </div>
+      <div className={`tabs ${currentTab !== 'Payment' ? 'hidded' : ''}`}>
+        Payment Form
+      </div>
     </div>
   );
 };

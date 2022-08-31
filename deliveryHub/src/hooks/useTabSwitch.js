4bacdbcdca4deff0057af0a77e9ed14679b73e74
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-31 16:06:37 -0300

    npm install react-hook-form and fix activeTab prop bug

--- deliveryHub/src/hooks/useTabSwitch.js
+++ deliveryHub/src/hooks/useTabSwitch.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 const useTabSwitch = (tabs, defaultTab) => {
   const [currentTab, setCurrentTab] = useState(defaultTab);
 
-  // every time the  default tab changes, we do want to set our current one to the default
+  // every time the defaultTag changes, we do want to set our current one to the default
   useEffect(() => {
     setCurrentTab(defaultTab);
   }, [defaultTab]);
@@ -13,24 +13,6 @@ const useTabSwitch = (tabs, defaultTab) => {
   };
 
   return [currentTab, handleTabSwitch];
-
-  // const [activeTab, setActiveTab] = useState(initialTab);
-  // const [activeTabIndex, setActiveTabIndex] = useState(0);
-  // const tabs = ["Summary", "Delivery", "Payment"];
-  // const onTabSwitch = (newActiveTab) => {
-  //     setActiveTab(newActiveTab);
-  //     let categories = products.products.map((product) => {
-  //     return product.name.name;
-  //     });
-  //     let index = categories.findIndex((category) => newActiveTab === category);
-  //     console.log(index);
-  //     if (index > -1) {
-  //     setActiveTabIndex(index);
-  //     } else {
-  //     setActiveTabIndex(0);
-  //     }
-  // };
-  // return { activeTab, activeTabIndex, tabs, onTabSwitch };
 };
 
 export default useTabSwitch;

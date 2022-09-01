fd15cb96adcf54ef7659e1a89abbdc17cb5ddac6
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-09-01 16:25:09 -0300

    make switch work by fixing bug on Cart component

--- deliveryHub/src/components/AddressForm.jsx
+++ deliveryHub/src/components/AddressForm.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import Button from './elements/Button';
+import { useDispatch } from 'react-redux';
+import { setAddress } from '../stores/userInfo/addressSlice';
 import { ReactComponent as ArrowRightSvg } from '../assets/icons/arrow-right-long-svgrepo-com.svg';
 
-const AddressForm = () => {
+const AddressForm = ({ onTabSwitch }) => {
   // register would save the values that the user inputs in the form
   const { register, handleSubmit } = useForm();
 
+  const dispatch = useDispatch();
+
   const onSubmit = (data) => {
-    console.log(data);
+    dispatch(setAddress(data));
+    onTabSwitch('Payment');
   };
 
   return (
--- deliveryHub/src/pages/Cart/index.jsx
+++ deliveryHub/src/pages/Cart/index.jsx
@@ -22,13 +22,13 @@ const Cart = () => {
   return (
     <div className="bg-white h-screen text-black mx-auto mt-2 border  border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
       <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab} />
-      <div className={`tabs ${currentTab !== 'Summary' ? 'hidded' : ''}`}>
+      <div className={`tabs ${currentTab !== 'Summary' ? 'hidden' : ''}`}>
         Summary
       </div>
-      <div className={`tabs ${currentTab !== 'Delivery' ? 'hidded' : ''}`}>
-        <AddressForm />
+      <div className={`tabs ${currentTab !== 'Delivery' ? 'hidden' : ''}`}>
+        <AddressForm onTabSwitch={handleTabSwitch} />
       </div>
-      <div className={`tabs ${currentTab !== 'Payment' ? 'hidded' : ''}`}>
+      <div className={`tabs ${currentTab !== 'Payment' ? 'hidden' : ''}`}>
         Payment Form
       </div>
     </div>

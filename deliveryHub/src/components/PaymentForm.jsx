223cba48d2fc0456edac337d01458e67a2ef8ca6
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-09-06 15:18:45 -0300

    work on PaymentForm HANDLESUBMIT

--- deliveryHub/src/components/PaymentForm.jsx
+++ deliveryHub/src/components/PaymentForm.jsx
@@ -5,8 +5,12 @@ import {
   useElements,
   Elements,
 } from '@stripe/react-stripe-js';
-
 import { loadStripe } from '@stripe/stripe-js';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart, cartProducts } from '../stores/cart/cartSlice';
+import { getAddress, clearAddress } from '../stores/userInfo/addressSlice';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
@@ -19,6 +23,60 @@ export const StripeWrapper = () => {
 };
 
 const PaymentForm = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const cart = useSelector(cartProducts);
+  const address = useSelector(getAddress);
+  const navigate = useNavigate();
+  const elements = useElements();
+  const stripe = useStripe();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements || !cart?.length || !address) {
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const { error: backeEndError, clientSecret } = await fetch(
+        'http://localhost:8080/create-payment-intent',
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            paymentMethodType: 'card',
+            orderItems: cart,
+            userId: '',
+            shippingAddress: address,
+          }),
+        }
+      ).then((r) => r.json());
+
+      const { error: stripeError, paymentIntent } =
+        await stripe.confirmCardPayment(clientSecret, {
+          payment_method: {
+            card: elements.getElement(CardElement),
+          },
+        });
+      if (backeEndError || stripeError) {
+        setError(backeEndError || stripeError);
+      } else if (paymentIntent.status === 'succeeded') {
+        dispatch(clearAddress());
+        dispatch(clearCart());
+        navigate('/payment-success');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <form className="md:-2/3 md:mx-auto px-2 pt-1" id="payment-form">
       <label htmlFor="card-element" className="pt-4 text-2xl md:text-center">

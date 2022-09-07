8891ae3c42d97700a8330d980b1e24d07d122d41
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-09-07 15:39:03 -0300

    2.0

--- deliveryHub/src/firebase-config.js
+++ deliveryHub/src/firebase-config.js
@@ -3,11 +3,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAzFZzSjLqa4hhepTq_-xoPJDpwHLhOtgY',
-  authDomain: 'food-delivery-sep-22.firebaseapp.com',
-  projectId: 'food-delivery-sep-22',
-  storageBucket: 'food-delivery-sep-22.appspot.com',
-  messagingSenderId: '253713289122',
-  appId: '1:253713289122:web:4890b51e3f2d0c85cc2cfb',
-  measurementId: 'G-H1DDSF1GHL',
+  apiKey: process.env.REACT_APP_FIREBASE_CONFIG_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_CONFIG_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_CONFIG_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_CONFIG_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_CONFIG_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_CONFIG_MEASUREMENT_ID,
 };
+
+export const app = initializeApp(firebaseConfig);

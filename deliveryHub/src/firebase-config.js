9ed5710582764c7c41cc199362fb132fd54e325f
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-09-07 15:26:11 -0300

    install firebase

---
+++ deliveryHub/src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzFZzSjLqa4hhepTq_-xoPJDpwHLhOtgY',
+  authDomain: 'food-delivery-sep-22.firebaseapp.com',
+  projectId: 'food-delivery-sep-22',
+  storageBucket: 'food-delivery-sep-22.appspot.com',
+  messagingSenderId: '253713289122',
+  appId: '1:253713289122:web:4890b51e3f2d0c85cc2cfb',
+  measurementId: 'G-H1DDSF1GHL',
+};
